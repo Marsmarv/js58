7c39757762bcc88dcb45620cd48598ae45cadecf
Marvin Romero <marvin.r02@gmail.com>
2017-05-17 04:31:39 -0400

    created hello.js

---
+++ 01/hello.js
@@ -0,0 +1,31 @@
+var x = 'Hello ';
+
+console.log(x + 'Marvin');
+console.log(x + 'Jack');
+
+var age = 65;
+
+if(age >= 21){
+	console.log("You may legally have a drink")
+}
+	if(age < 25) {
+	 console.log("You probably want Jaeger")
+	}
+		if(age >= 25 && age < 60) {
+		 console.log("You want an old fashioned")
+		}
+			if(age >= 60) {
+	 		 console.log("You want to drink Brandy")
+			}
+
+var counter = 1
+while(counter < 10) {
+	console.log("woohoo " + counter)
+	
+	if(counter % 3 == 0) {
+		break
+	}
+	counter++
+}
+
+counter++
