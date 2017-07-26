a204ff7749788705810029ad37b22560ad0098f9
Marvin R <marvin.r02@gmail.com>
2017-07-25 21:27:15 -0400

    upload

--- js-homework/HWclass07/HW07.js
+++ js-homework/HWclass07/HW07.js
@@ -1,22 +1,22 @@
-$ (function() {
-	var $go = $('go-button')
-	var $button = $('go')
+$(function() {
+	console.log('123')
+
+	var $input = $('#go-input')
+	var $button = $('.go-btn')
 
 	$button.on('click', function(e){
-	// e.preventDefault()
+		e.preventDefault()
+	 	  var x = $input.val();
+	  console.log(x)
 
-	var  
-
-		for(var i = 0; i < 100; i++) {
-		  var v = "";
-		  v += (i % 3 == 0) ? "Fizz" : "";
-		  v += (i % 5 == 0) ? "Buzz" : "";
-		  if(v.length == 0) {
-		    console.log(i)
-		  } else {
-		    console.log(v)
-		  }
+		for(var i = 0 ; i < x; i++) {
+			if (i % 3 == 0) {
+				console.log("Fizz")
+			} else if (i % 5 == 0) {
+				console.log("Buzz")
+			} else {console.log(i)}
 		}
 	
 	})
 })
+
